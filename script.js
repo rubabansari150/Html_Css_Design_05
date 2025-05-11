e0b213be608a260ee6cbb2eb943868e0b5bf2e9c
rubabansari150 <142120983+rubabansari150@users.noreply.github.com>
2025-05-11 13:12:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from("#img_1 ", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: 60,
+});
+gsap.from("#img_2 ", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  x: 60,
+});
+gsap.from("#img_3 ", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+  y: -60,
+});
+gsap.from("#main h1 ", {
+  opacity: 0,
+  duration: 1,
+  delay: 0.4,
+});
